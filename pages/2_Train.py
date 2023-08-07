15da2bcd31ef36f88ef4b62beb0a9e0a5697465d
Ketan Suhaas Saichandran <55935983+Ketansuhaas@users.noreply.github.com>
2023-08-07 17:15:19 +0530

    Update 2_Train.py

--- pages/2_Train.py
+++ pages/2_Train.py
@@ -12,6 +12,8 @@ st.title("Train")
 # print model information
 
 file = st.file_uploader("Choose your .txt file")
+if file is not None:
+    raw = str(file.read(),"utf-8")
 if (st.button("Train")):
 
     x_train, y_train, vocab_size = preprocess_context(raw)
@@ -19,9 +21,8 @@ if (st.button("Train")):
     model.fit(x_train,y_train, epochs=1000, batch_size = 32, verbose=1)
     model.save("first_model.h5")
     st.session_state.model = model
+st.write(raw)
 
 
-if file is not None:
-    raw = str(file.read(),"utf-8")
-    st.write(raw)
+
 
